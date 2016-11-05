9a9e13bb61c0f157cd76fbdfcaeb10ff2096e012
polis89 <89polis@gmail.com>
2016-11-05 16:38:12 +0100

    Add boundarys and shotsRemovedByBoundarys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,25 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float tilt;
+	public GameObject shot;
+	public Transform shotSpawnTransform;
 	public Boundary boundary;
+	public float fireRate;
+
 	private Rigidbody rigitbody;
+	private float nextFire = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		rigitbody = GetComponent<Rigidbody>();
+	}
+
+	void Update(){
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate (shot, shotSpawnTransform.position, shotSpawnTransform.rotation);// as GameObject;
+		}
 	}
 
 	void FixedUpdate () {

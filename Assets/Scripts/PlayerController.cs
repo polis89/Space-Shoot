094c193c52f0eab17af02228b144ef82e503ea3b
polis89 <89polis@gmail.com>
2016-11-05 12:47:19 +0100

    Add player moves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	private Rigidbody rigitbody;
+
+	// Use this for initialization
+	void Start () {
+		rigitbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float moveHorisontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 vector = new Vector3 (moveHorisontal, 0.0f, moveVertical);
+		rigitbody.velocity = vector * speed;
+		rigitbody.position = new Vector3 (
+			Mathf.Clamp(rigitbody.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(rigitbody.position.z, boundary.yMin, boundary.yMax)
+		);
+		rigitbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigitbody.velocity.x * -tilt);
+	}
+}
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, yMin, yMax;
+}

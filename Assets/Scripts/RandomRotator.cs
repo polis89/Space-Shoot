d119b5bc3f8cb75c94f7e11d0fed4c73484cca22
polis89 <89polis@gmail.com>
2016-11-06 19:42:52 +0100

    Add asteroid with collider

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble;
+
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

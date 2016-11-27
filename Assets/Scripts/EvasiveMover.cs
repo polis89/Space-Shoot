7a052a4dfad3bf6c399373e206c6a566c14871dd
polis89 <89polis@gmail.com>
2016-11-27 13:43:35 +0100

    Complete with enemies

--- Assets/Scripts/EvasiveMover.cs
+++ Assets/Scripts/EvasiveMover.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class EvasiveMover : MonoBehaviour {
 	
 	public float dodge;
+	public float smoothing;
+	public float tilt;
 	public Vector2 startWait;
 	public Vector2 manouverTime;
 	public Vector2 manouverWait;
+	public Boundary boundary;
 
 	private float targetManeuver;
+	private float currentSpeed;
+	private Rigidbody rb;
 
 	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		currentSpeed = rb.velocity.z;
 		StartCoroutine (Evade ());
 	}
 
@@ -18,14 +25,20 @@ public class EvasiveMover : MonoBehaviour {
 		yield return new WaitForSeconds (Random.Range (startWait.x, startWait.y));
 
 		while (true) {
-			targetManeuver = Random.Range (1, dodge) * -Mathf.Sign;
-			yield return new WaitForSeconds ();
+			targetManeuver = Random.Range (1, dodge) * -Mathf.Sign(transform.position.x);
+			yield return new WaitForSeconds (Random.Range(manouverTime.x, manouverTime.y));
 			targetManeuver = 0;
-			yield return new WaitForSeconds ();
+			yield return new WaitForSeconds (Random.Range(manouverWait.x, manouverTime.y));
 		}
 	}
 
-	void Update () {
-	
+	void FixedUpdate () {
+		float newManeuver = Mathf.MoveTowards (rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+		rb.velocity = new Vector3 (newManeuver, 0.0f, currentSpeed);
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (rb.position.z, boundary.yMin, boundary.yMax));
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
